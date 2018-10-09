0b536c02787e9254f8759e4881cf0229a36ecb92
robear <rob@robmagee.com.au>
2018-10-10 09:29:41 +1100

    Update Both arduinos to drive independent motors

--- Hexa_Brushless_test_knob_Ard1_02/Hexa_Brushless_test_knob_Ard1_02.ino
+++ Hexa_Brushless_test_knob_Ard1_02/Hexa_Brushless_test_knob_Ard1_02.ino
@@ -7,11 +7,12 @@
  http://www.arduino.cc/en/Tutorial/Knob
 */
 
+// ++++++ ARDUINO #1 ++++++
+
 #include <Servo.h>
 
-Servo esc2;
-Servo esc3;  // create servo object to control a servo
-Servo esc4;
+Servo esc1;  // create servo object to control a servo
+Servo esc3;  // 1, 3, 5 are driven by Arduino 1 (2,4,6 by Arduino 2)
 Servo esc5;
 
 
@@ -19,25 +20,26 @@ int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup() {
-  esc2.attach(2);
+  esc1.attach(1);
   esc3.attach(3);
-  esc4.attach(4);  // attaches the servo on pin 4 to the servo object
-  esc5.attach(5);
+  esc5.attach(5);  // attaches the servo on pin 5 to the servo object
 
-  esc2.writeMicroseconds(1000);
+  esc1.writeMicroseconds(1000);
   esc3.writeMicroseconds(1000); //initialise to 1000
-  esc4.writeMicroseconds(1000);
   esc5.writeMicroseconds(1000);
+
   Serial.begin(9600);
 }
 
 void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 1023, 1000, 2000);     // scale it to use it with the servo (value between 0 and 180)
-  esc2.writeMicroseconds(val);
+  esc1.writeMicroseconds(val);
   esc3.writeMicroseconds(val);                  // sets the servo position according to the scaled value
-  esc4.writeMicroseconds(val);
   esc5.writeMicroseconds(val);
+
   //delay(15);                           // waits for the servo to get there
 }
+
+
 
--- Hexa_Brushless_test_knob_Ard2_02/Hexa_Brushless_test_knob_Ard2_02.ino
+++ Hexa_Brushless_test_knob_Ard2_02/Hexa_Brushless_test_knob_Ard2_02.ino
@@ -7,12 +7,13 @@
  http://www.arduino.cc/en/Tutorial/Knob
 */
 
+// ++++++ ARDUINO #2 ++++++
+
 #include <Servo.h>
 
-Servo esc2;
-Servo esc3;  // create servo object to control a servo
-Servo esc4;
-Servo esc5;
+Servo esc2;  // create servo object to control a servo
+Servo esc4;  // 2, 4, 6 are driven by Arduino 2 (1,3,5 by Arduino 1)
+Servo esc6;
 
 
 int potpin = 0;  // analog pin used to connect the potentiometer
@@ -20,14 +21,13 @@ int val;    // variable to read the value from the analog pin
 
 void setup() {
   esc2.attach(2);
-  esc3.attach(3);
-  esc4.attach(4);  // attaches the servo on pin 4 to the servo object
-  esc5.attach(5);
+  esc4.attach(4);
+  esc6.attach(6);  // attaches the servo on pin 4 to the servo object
 
   esc2.writeMicroseconds(1000);
-  esc3.writeMicroseconds(1000); //initialise to 1000
-  esc4.writeMicroseconds(1000);
-  esc5.writeMicroseconds(1000);
+  esc4.writeMicroseconds(1000); //initialise to 1000
+  esc6.writeMicroseconds(1000);
+  
   Serial.begin(9600);
 }
 
@@ -35,9 +35,9 @@ void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 1023, 1000, 2000);     // scale it to use it with the servo (value between 0 and 180)
   esc2.writeMicroseconds(val);
-  esc3.writeMicroseconds(val);                  // sets the servo position according to the scaled value
-  esc4.writeMicroseconds(val);
-  esc5.writeMicroseconds(val);
+  esc4.writeMicroseconds(val);                  // sets the servo position according to the scaled value
+  esc6.writeMicroseconds(val);
+
   //delay(15);                           // waits for the servo to get there
 }
 
