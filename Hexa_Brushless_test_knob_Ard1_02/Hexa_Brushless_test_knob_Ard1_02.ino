21963f47e04f9caeecac97be5611c618b6150135
robear <rob@robmagee.com.au>
2018-10-14 08:27:13 +1100

    Change to servo for multi motors instead of ESC library

--- Hexa_Brushless_test_knob_Ard1_02/Hexa_Brushless_test_knob_Ard1_02.ino
+++ Hexa_Brushless_test_knob_Ard1_02/Hexa_Brushless_test_knob_Ard1_02.ino
@@ -10,36 +10,127 @@
 // ++++++ ARDUINO #1 ++++++ UNO ++++++
 
 #include <Servo.h>
+//#include <ESC.h>
 
+#define LED_PIN (13)
+#define POT_PIN (A0) //Analog pin used to connect the potentiometer
+
+//Create the 4 esc objects
 Servo esc1;  // create servo object to control a servo
 Servo esc3;  // 1, 3, 5 are driven by Arduino 1 (2,4,6 by Arduino 2)
 Servo esc5;
 
 
-int potpin = 0;  // analog pin used to connect the potentiometer
+//ESC myESC_1 (3, 1000, 2000, 500); // ESC_Name (PIN, Minimum Value, Maximum Value, Arm Value)
+//ESC myESC_3 (5, 1000, 2000, 500);
+//ESC myESC_5 (6, 1000, 2000, 500);
+
+//int potpin = 0;  // analog pin used to connect the potentiometer
+int escPin1 = 3;
+int escPin3 = 5;
+int escPin5 = 6;
+int minPulseRate = 1000;
+int maxPulseRate = 2000;
+int throttleChangeDelay = 50;
+
 int val;    // variable to read the value from the analog pin
 
 void setup() {
-  esc1.attach(3);
-  esc3.attach(5);
-  esc5.attach(6);  // attaches the servo on pin 6 to the servo object
-
-  esc1.writeMicroseconds(1000);
-  esc3.writeMicroseconds(1000); //initialise to 1000
-  esc5.writeMicroseconds(1000);
-
   Serial.begin(9600);
+  Serial.setTimeout(500);
+
+  initEscs();
+  //esc1.attach(3);
+  //esc3.attach(5);
+  //esc5.attach(6);  // attaches the servo on pin 6 to the servo object
+
+  //esc1.writeMicroseconds(1000);
+  //esc3.writeMicroseconds(1000); //initialise to 1000
+  //esc5.writeMicroseconds(1000);
+
+  //pinMode(LED_PIN, OUTPUT);             // LED Visual Output
+  //myESC_1.arm();                          // Send the Arm value
+  //myESC_3.arm();
+  //myESC_5.arm();
+  //digitalWrite(LED_PIN, HIGH);          // LED High Once Armed
+  delay(5000);                          // Wait for a while
+  
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = analogRead(POT_PIN);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 1023, 1000, 2000);     // scale it to use it with the servo (value between 0 and 180)
-  esc1.writeMicroseconds(val);
-  esc3.writeMicroseconds(val);                  // sets the servo position according to the scaled value
-  esc5.writeMicroseconds(val);
+  
+  //int throttle = normaliseThrottle(val);
+  //changeThrottle(throttle);
+  writeTo3Escs(val);
+  
+  //esc1.writeMicroseconds(val);
+  //esc3.writeMicroseconds(val);                  // sets the servo position according to the scaled value
+  //esc5.writeMicroseconds(val);
 
-  //delay(15);                           // waits for the servo to get there
+  //myESC_1.speed(val);
+  //myESC_3.speed(val);
+  //myESC_5.speed(val);
+  //delay(5);
+
+  delay(15);                           // waits for the servo to get there
 }
 
 
+
+//Change throttle value
+//void changeThrottle(int throttle) {
+ // int currentThrottle = readThrottle();
+  
+ // int step = 1;
+  //if(throttle < currentThrottle) {
+ //   step = -1;
+ // }
+  
+  // Slowly move to the new throttle value 
+ // while(currentThrottle != throttle) {
+ //   writeTo4Escs(currentThrottle + step);
+    
+ //   currentThrottle = readThrottle();
+  //  delay(throttleChangeDelay);
+ // }
+//}
+
+
+//Change velocity of the 4 escs at the same time
+void writeTo3Escs(int throttle) {
+  esc1.write(throttle);
+  esc3.write(throttle);
+  esc5.write(throttle);
+}
+
+
+//Init escs
+void initEscs() {
+  esc1.attach(escPin1, minPulseRate, maxPulseRate);
+  esc3.attach(escPin3, minPulseRate, maxPulseRate);
+  esc5.attach(escPin5, minPulseRate, maxPulseRate);
+  
+  
+  //Init motors with 0 value
+  writeTo3Escs(0);
+}
+
+//Start the motors
+void startUpMotors() {
+  writeTo3Escs(50);
+}
+
+// Ensure the throttle value is between 0 - 180
+int normalizeThrottle(int value) {
+  if(value < 0) {
+    return 0;
+    
+  } else if(value > 180) {
+    return 180;
+  }
+  
+  return value;
+}
 
--- Hexa_Brushless_test_knob_Ard2_02/Hexa_Brushless_test_knob_Ard2_02.ino
+++ Hexa_Brushless_test_knob_Ard2_02/Hexa_Brushless_test_knob_Ard2_02.ino
@@ -9,35 +9,106 @@
 
 // ++++++ ARDUINO #2 ++++++ NANO +++++++
 
-//#include <Servo.h>
+#include <Servo.h>
+//#include <ESC.h>
+
+#define LED_PIN (13)
+#define POT_PIN (A0) //Analog pin used to connect the potentiometer
 
 Servo esc2;  // create servo object to control a servo
 Servo esc4;  // 2, 4, 6 are driven by Arduino 2 (1,3,5 by Arduino 1)
 Servo esc6;
 
+//ESC myESC_2 (9, 1000, 2000, 500); // ESC_Name (PIN, Minimum Value, Maximum Value, Arm Value)
+//ESC myESC_4 (10, 1000, 2000, 500);
+//ESC myESC_6 (11, 1000, 2000, 500);
 
-int potpin = 0;  // analog pin used to connect the potentiometer
+// int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
+int escPin2 = 9;
+int escPin4 = 10;
+int escPin6 = 11;
+int minPulseRate = 1000;
+int maxPulseRate = 2000;
+int throttleChangeDelay = 50;
 
 void setup() { // attaches the servo on pin 11 to the servo object
-  esc2.attach(9);   // 2 = Green
-  esc4.attach(10);  // 4 = Green stripe
-  esc6.attach(11);  // 6 = Orange stripe
+  initEscs();
+  //esc2.attach(9);   // 2 = Green
+  //esc4.attach(10);  // 4 = Green stripe
+  //esc6.attach(11);  // 6 = Orange stripe
 
-  esc2.writeMicroseconds(1000);
-  esc4.writeMicroseconds(1000); //initialise to 1000
-  esc6.writeMicroseconds(1000);
+  //esc2.writeMicroseconds(1000);
+  //esc4.writeMicroseconds(1000); //initialise to 1000
+  //esc6.writeMicroseconds(1000);
   
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  //pinMode(LED_PIN, OUTPUT);             // LED Visual Output
+  //myESC_2.arm();                          // Send the Arm value
+  //myESC_4.arm();
+  //myESC_6.arm();
+  //digitalWrite(LED_PIN, HIGH);          // LED High Once Armed
+  delay(5000);                          // Wait for a while
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = analogRead( POT_PIN );            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 1023, 1000, 2000);     // scale it to use it with the servo (value between 0 and 180)
-  esc2.writeMicroseconds(val);
-  esc4.writeMicroseconds(val);                  // sets the servo position according to the scaled value
-  esc6.writeMicroseconds(val);
+  
+  writeTo3Escs(val);
+  //myESC_2.speed(val);
+  //myESC_4.speed(val);
+  //myESC_6.speed(val);
+  
+  delay(15);
+  
+  //esc2.writeMicroseconds(val);
+  //esc4.writeMicroseconds(val);                  // sets the servo position according to the scaled value
+  //esc6.writeMicroseconds(val);
 
   //delay(15);                           // waits for the servo to get there
 }
+
+
+//Change velocity of the 4 escs at the same time
+void writeTo3Escs(int throttle) {
+  esc2.write(throttle);
+  esc4.write(throttle);
+  esc6.write(throttle);
+}
+
+
+//Init escs
+void initEscs() {
+  esc2.attach(escPin2, minPulseRate, maxPulseRate);
+  esc4.attach(escPin4, minPulseRate, maxPulseRate);
+  esc6.attach(escPin6, minPulseRate, maxPulseRate);
+  
+  //Init motors with 0 value
+  writeTo3Escs(0);
+}
+
+//Start the motors
+void startUpMotors() {
+  writeTo3Escs(50);
+}
+
+// Ensure the throttle value is between 0 - 180
+int normalizeThrottle(int value) {
+  if(value < 0) {
+    return 0;
+    
+  } else if(value > 180) {
+    return 180;
+  }
+  
+  return value;
+}
+
+
+
+
+
+
+
 
